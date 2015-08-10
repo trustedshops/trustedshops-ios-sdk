bab42c6e542011cea27193656e828c0d21c2c940
Marc Kalmes <mkalmes@mkalmes.net>
2015-08-10 15:49:54 +0200

    Add Attributed String

--- Pod/Classes/TRSTrustbadgeView.m
+++ Pod/Classes/TRSTrustbadgeView.m
@@ -7,6 +7,7 @@
 @interface TRSTrustbadgeView ()
 
 @property (nonatomic, copy, readwrite) NSString *trustedShopsID;
+@property (nonatomic, strong) NSNumberFormatter *decimalFormatter;
 
 @end
 
@@ -24,7 +25,10 @@
         TRSRatingView *ratingView = [[TRSRatingView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 86.0f, 18.0f)
                                                                   rating:trustbadge.rating];
 
+        UILabel *ratingLabel = [self labelForRating:trustbadge.rating];
+
         [self addSubview:ratingView];
+        [self addSubview:ratingLabel];
     };
 
     void (^failure)(NSError *error) = ^(NSError *error) {
@@ -37,6 +41,47 @@
                                   failure:failure];
 
     return self;
+}
+
+- (NSNumberFormatter *)decimalFormatter {
+    if (!_decimalFormatter) {
+        NSNumberFormatter *decimalFormatter = [[NSNumberFormatter alloc] init];
+        decimalFormatter.minimumFractionDigits = 2;
+        decimalFormatter.maximumFractionDigits = 2;
+        _decimalFormatter = decimalFormatter;
+    }
+
+    return _decimalFormatter;
+}
+
+- (UILabel *)labelForRating:(NSNumber *)rating {
+    UILabel *ratingLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 80.0f, 18.0f)];
+    ratingLabel.attributedText = [self ratingStringForRating:rating];
+    return ratingLabel;
+}
+
+- (NSAttributedString *)ratingStringForRating:(NSNumber *)rating {
+    NSString *maxRatingString = [NSString stringWithFormat:@"/%@", [self.decimalFormatter stringFromNumber:@5.0]];
+    NSString *ratingString = [NSString stringWithFormat:@"%@%@", rating, maxRatingString];
+
+    UIFont *normalFont = [UIFont fontWithName:@"Arial" size:14.0];
+    NSDictionary *attributes = @{ NSFontAttributeName : normalFont };
+    NSMutableAttributedString *attributedRatingString = [[NSMutableAttributedString alloc] initWithString:ratingString
+                                                                                               attributes:attributes];
+
+    UIFont *smallFont = [UIFont fontWithName:@"Arial" size:12.0];
+    UIColor *grayColor = [UIColor grayColor];
+    NSRange range = [ratingString rangeOfString:maxRatingString];
+
+    [attributedRatingString addAttribute:NSFontAttributeName
+                                   value:smallFont
+                                   range:range];
+
+    [attributedRatingString addAttribute:NSForegroundColorAttributeName
+                                   value:grayColor
+                                   range:range];
+
+    return [attributedRatingString copy];
 }
 
 @end
