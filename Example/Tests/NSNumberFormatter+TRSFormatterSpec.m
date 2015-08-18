65043d9405c115c6b109b3eee14d828edd72a4a9
Marc Kalmes <mkalmes@mkalmes.net>
2015-08-18 13:13:26 +0200

    Remove Unneeded `NSLocale` Stubs
    The new behavior is not affected by `NSLocale`. The stubs can be safely
    deleted.

--- Example/Tests/NSNumberFormatter+TRSFormatterSpec.m
+++ Example/Tests/NSNumberFormatter+TRSFormatterSpec.m
@@ -19,30 +19,6 @@ describe(@"NSNumberFormatter+TRSFormatter", ^{
         rating = nil;
     });
 
-    sharedExamplesFor(@"decimal seperator", ^(NSDictionary *data) {
-
-        it(@"it uses a `.` as a decimal seperator", ^{
-            expect(data[@"formattedNumberString"]).to.equal(@"4.42");
-        });
-
-    });
-
-    sharedExamplesFor(@"integer digits", ^(NSDictionary *data) {
-
-        it(@"it formats the number with one integer digit", ^{
-            expect(data[@"formattedNumberString"]).to.equal(@"4");
-        });
-
-    });
-
-    sharedExamplesFor(@"fraction digits", ^(NSDictionary *data) {
-
-        it(@"it formats the number with two fraction digits", ^{
-            expect(data[@"formattedNumberString"]).to.equal(@"42");
-        });
-
-    });
-
     describe(@"+trs_trustbadgeRatingFormatter", ^{
 
         it(@"returns a `NSNumberFormatter` object", ^{
@@ -54,97 +30,18 @@ describe(@"NSNumberFormatter+TRSFormatter", ^{
             expect(formatter).to.equal(otherFormatter);
         });
 
-        context(@"for en_US locale", ^{
-
-            __block id localeMock;
-            before(^{
-                NSLocale *usLocale = [NSLocale localeWithLocaleIdentifier:@"en_US"];
-                localeMock = OCMClassMock([NSLocale class]);
-                OCMStub([localeMock currentLocale]).andReturn(usLocale);
-            });
-
-            after(^{
-                localeMock = nil;
-            });
-
-            itShouldBehaveLike(@"decimal seperator", ^{
-                return @{@"formattedNumberString" : [formatter stringFromNumber:rating]};
-            });
-
-            itShouldBehaveLike(@"integer digits", ^{
-                NSString *formattedNumberString = [formatter stringFromNumber:rating];
-
-                return @{@"formattedNumberString" : [[formattedNumberString componentsSeparatedByString:@"."] firstObject]};
-            });
-
-            itShouldBehaveLike(@"fraction digits", ^{
-                NSString *formattedNumberString = [formatter stringFromNumber:rating];
-
-                return @{@"formattedNumberString" : [[formattedNumberString componentsSeparatedByString:@"."] lastObject]};
-            });
-
+        it(@"it uses a `.` as a decimal seperator", ^{
+            expect([formatter stringFromNumber:rating]).to.equal(@"4.42");
         });
 
-        context(@"for de_DE locale", ^{
-
-            __block id localeMock;
-            before(^{
-                NSLocale *deLocale = [NSLocale localeWithLocaleIdentifier:@"de_DE"];
-                localeMock = OCMClassMock([NSLocale class]);
-                OCMStub([localeMock currentLocale]).andReturn(deLocale);
-            });
-
-            after(^{
-                localeMock = nil;
-            });
-
-            itShouldBehaveLike(@"decimal seperator", ^{
-                return @{@"formattedNumberString" : [formatter stringFromNumber:rating]};
-            });
-
-            itShouldBehaveLike(@"integer digits", ^{
-                NSString *formattedNumberString = [formatter stringFromNumber:rating];
-
-                return @{@"formattedNumberString" : [[formattedNumberString componentsSeparatedByString:@"."] firstObject]};
-            });
-
-            itShouldBehaveLike(@"fraction digits", ^{
-                NSString *formattedNumberString = [formatter stringFromNumber:rating];
-
-                return @{@"formattedNumberString" : [[formattedNumberString componentsSeparatedByString:@"."] lastObject]};
-            });
-
+        it(@"it formats the number with one integer digit", ^{
+            NSString *formattedNumberString = [[[formatter stringFromNumber:rating] componentsSeparatedByString:@"."] firstObject];
+            expect(formattedNumberString).to.equal(@"4");
         });
 
-        context(@"for es_ES locale", ^{
-
-            __block id localeMock;
-            before(^{
-                NSLocale *esLocale = [NSLocale localeWithLocaleIdentifier:@"es_ES"];
-                localeMock = OCMClassMock([NSLocale class]);
-                OCMStub([localeMock currentLocale]).andReturn(esLocale);
-            });
-
-            after(^{
-                localeMock = nil;
-            });
-
-            itShouldBehaveLike(@"decimal seperator", ^{
-                return @{@"formattedNumberString" : [formatter stringFromNumber:rating]};
-            });
-
-            itShouldBehaveLike(@"integer digits", ^{
-                NSString *formattedNumberString = [formatter stringFromNumber:rating];
-
-                return @{@"formattedNumberString" : [[formattedNumberString componentsSeparatedByString:@"."] firstObject]};
-            });
-
-            itShouldBehaveLike(@"fraction digits", ^{
-                NSString *formattedNumberString = [formatter stringFromNumber:rating];
-
-                return @{@"formattedNumberString" : [[formattedNumberString componentsSeparatedByString:@"."] lastObject]};
-            });
-
+        it(@"it formats the number with two fraction digits", ^{
+            NSString *formattedNumberString = [[[formatter stringFromNumber:rating] componentsSeparatedByString:@"."] lastObject];
+            expect(formattedNumberString).to.equal(@"42");
         });
 
     });
