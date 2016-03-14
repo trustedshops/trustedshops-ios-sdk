e91ac5b599462bf84494de3c6d6980b140b7f1c5
Gero Herkenrath <gero.herkenrath@trustedshops.de>
2016-03-14 09:57:54 +0100

    set up class to display dialogue, added font

---
+++ Pod/Classes/Private/TRSTrustcard.h
@@ -0,0 +1,13 @@
+//
+//  TRSTrustcard.h
+//  Pods
+//
+//  Created by Gero Herkenrath on 14.03.16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TRSTrustcard : NSObject
+
+@end
---
+++ Pod/Classes/Private/TRSTrustcard.m
@@ -0,0 +1,34 @@
+//
+//  TRSTrustcard.m
+//  Pods
+//
+//  Created by Gero Herkenrath on 14.03.16.
+//
+//
+
+#import "TRSTrustcard.h"
+
+@interface TRSTrustcard ()
+
+@property (nonatomic, weak) IBOutlet UIView *cardView;
+@property (weak, nonatomic) IBOutlet UIImageView *sealImage;
+
+@property (weak, nonatomic) IBOutlet UILabel *cardHeader;
+
+@property (nonatomic, weak) IBOutlet UILabel *checkmark;
+@property (weak, nonatomic) IBOutlet UILabel *checkHeader;
+@property (weak, nonatomic) IBOutlet UILabel *checkText;
+
+@property (nonatomic, weak) IBOutlet UILabel *lockmark;
+@property (weak, nonatomic) IBOutlet UILabel *lockHeader;
+@property (weak, nonatomic) IBOutlet UILabel *lockText;
+
+@property (nonatomic, weak) IBOutlet UILabel *bubblesmark;
+@property (weak, nonatomic) IBOutlet UILabel *bubblesHeader;
+@property (weak, nonatomic) IBOutlet UILabel *bubblesText;
+
+@end
+
+@implementation TRSTrustcard
+
+@end
