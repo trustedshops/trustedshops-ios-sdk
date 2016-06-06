2c719d80ee090afbcad8d6a56a30ef5ac24a3749
Gero Herkenrath <gero.herkenrath@trustedshops.de>
2016-06-06 17:06:13 +0200

    updated cocoapods, re-included snapshot tests

--- Example/Tests/TRSRatingViewSpec.m
+++ Example/Tests/TRSRatingViewSpec.m
@@ -1,6 +1,6 @@
 #import "TRSRatingView.h"
 #import <Expecta/Expecta.h>
-//#import <Expecta_Snapshots/EXPMatchers+FBSnapshotTest.h>
+#import <Expecta_Snapshots/EXPMatchers+FBSnapshotTest.h>
 #import <Specta/Specta.h>
 
 
@@ -10,7 +10,7 @@ describe(@"TRSRatingView", ^{
 
     describe(@"-initWithFrame:rating:", ^{
 
-        it(@"returns an initialized object", ^{
+		it(@"returns an initialized object", ^{
             TRSRatingView *view = [[TRSRatingView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 64.0f, 32.0f) rating:@1];
             expect(view).toNot.beNil();
 			expect(view).to.beKindOf([TRSRatingView class]);
@@ -20,22 +20,22 @@ describe(@"TRSRatingView", ^{
         it(@"returns a view with non stars", ^{
             TRSRatingView *view = [[TRSRatingView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 64.0f, 32.0f) rating:@0];
 			expect(view).to.beKindOf([TRSRatingView class]);
-//             expect(view).to.recordSnapshot();
-//            expect(view).to.haveValidSnapshot();
+//			expect(view).to.recordSnapshot();
+			expect(view).to.haveValidSnapshot();
         });
 
         it(@"returns a view with two and a half stars", ^{
             TRSRatingView *view = [[TRSRatingView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 64.0f, 32.0f) rating:@2.5];
 			expect(view).to.beKindOf([TRSRatingView class]);
-//             expect(view).to.recordSnapshot();
-//            expect(view).to.haveValidSnapshot();
+//			expect(view).to.recordSnapshot();
+			expect(view).to.haveValidSnapshot();
         });
 
         it(@"returns a view with five stars", ^{
             TRSRatingView *view = [[TRSRatingView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 64.0f, 32.0f) rating:@5];
 			expect(view).to.beKindOf([TRSRatingView class]);
-//             expect(view).to.recordSnapshot();
-//            expect(view).to.haveValidSnapshot();
+//			expect(view).to.recordSnapshot();
+			expect(view).to.haveValidSnapshot();
         });
 
     });
--- Example/Tests/TRSStarViewSpec.m
+++ Example/Tests/TRSStarViewSpec.m
@@ -1,6 +1,6 @@
 #import "TRSStarView.h"
 #import <Expecta/Expecta.h>
-//#import <Expecta_Snapshots/EXPMatchers+FBSnapshotTest.h>
+#import <Expecta_Snapshots/EXPMatchers+FBSnapshotTest.h>
 #import <Specta/Specta.h>
 
 
@@ -13,8 +13,8 @@ describe(@"TRSStarView", ^{
         it(@"returns a filled star", ^{
             TRSStarView *view = [TRSStarView filledStarWithSize:CGSizeMake(64.0f, 64.0f)];
 			expect(view).to.beKindOf([TRSStarView class]);
-//             expect(view).to.recordSnapshot();
-//            expect(view).to.haveValidSnapshot();
+//			expect(view).to.recordSnapshot();
+			expect(view).to.haveValidSnapshot();
         });
 
     });
@@ -24,8 +24,8 @@ describe(@"TRSStarView", ^{
         it(@"returns an empty star", ^{
             TRSStarView *view = [TRSStarView emptyStarWithSize:CGSizeMake(64.0f, 64.0f)];
 			expect(view).to.beKindOf([TRSStarView class]);
-//             expect(view).to.recordSnapshot();
-//            expect(view).to.haveValidSnapshot();
+//			expect(view).to.recordSnapshot();
+			expect(view).to.haveValidSnapshot();
         });
 
     });
@@ -35,8 +35,8 @@ describe(@"TRSStarView", ^{
         it(@"returns a half-filled star", ^{
             TRSStarView *view = [[TRSStarView alloc ] initWithSize:CGSizeMake(64.0f, 64.0f) percentFilled:@0.5];
 			expect(view).to.beKindOf([TRSStarView class]);
-//             expect(view).to.recordSnapshot();
-//            expect(view).to.haveValidSnapshot();
+//			expect(view).to.recordSnapshot();
+			expect(view).to.haveValidSnapshot();
         });
 
     });
