c7d306e5d5b3747eb8a7832d7173bda856bced9a
Gero Herkenrath <gero.herkenrath@trustedshops.de>
2016-06-07 14:01:48 +0200

    added a scene in example target to show view objects

---
+++ Example/Trustbadge/TRSViewGalleryController.h
@@ -0,0 +1,13 @@
+//
+//  TRSViewGalleryController.h
+//  Trustbadge
+//
+//  Created by Gero Herkenrath on 07/06/16.
+//  Copyright © 2016 Trusted Shops GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRSViewGalleryController : UIViewController
+
+@end
---
+++ Example/Trustbadge/TRSViewGalleryController.m
@@ -0,0 +1,78 @@
+//
+//  TRSViewGalleryController.m
+//  Trustbadge
+//
+//  Created by Gero Herkenrath on 07/06/16.
+//  Copyright © 2016 Trusted Shops GmbH. All rights reserved.
+//
+
+#import "TRSViewGalleryController.h"
+@import Trustbadge.TRSTrustbadgeView;
+
+@interface TRSViewGalleryController ()
+
+@property (nonatomic, strong) NSMutableArray *loadedViews;
+@property (nonatomic, assign) NSUInteger loadingViewsCount;
+
+@property (weak, nonatomic) IBOutlet UIView *sealPlaceholder;
+- (IBAction)loadSeal:(id)sender;
+
+@end
+
+@implementation TRSViewGalleryController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+	
+	self.loadedViews = [NSMutableArray new];
+	self.loadingViewsCount = 0;
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+	[super viewWillDisappear:animated];
+	
+	// remove all loaded views
+	for (UIView *view in self.loadedViews) {
+		[view removeFromSuperview];
+	}
+}
+
+
+- (IBAction)loadSeal:(id)sender {
+	NSString *chDemoTSID = @"X330A2E7D449E31E467D2F53A55DDD070";
+	CGRect tbFrame = self.sealPlaceholder.frame;
+	tbFrame.origin.x = 0.0;
+	tbFrame.origin.y = 0.0;
+	TRSTrustbadgeView *tbView = [[TRSTrustbadgeView alloc] initWithFrame:tbFrame trustedShopsID:chDemoTSID apiToken:@"N/A"];
+	[self willLoadAView];
+	[tbView loadTrustbadgeWithSuccessBlock:^{
+		[self didLoadView:tbView forParent:self.sealPlaceholder];
+	} failureBlock:^(NSError *error) {
+		NSLog(@"could not load TRSTrustbadgeView, error: %@", error);
+		[self didLoadView:tbView forParent:nil];
+	}];
+}
+
+- (void)willLoadAView {
+	self.loadingViewsCount++;
+	if (self.loadingViewsCount > 0) {
+		// prevent prematurely allowing to go back (before loading requests have finished)
+		UIViewController *neighbor = [self.tabBarController.viewControllers firstObject];
+		neighbor.tabBarItem.enabled = NO;
+	}
+}
+
+- (void)didLoadView:(UIView *)view forParent:(UIView *)parent {
+	if (parent) { // on success
+		[self.loadedViews addObject:view];
+		[parent addSubview:view];
+	}
+	
+	self.loadingViewsCount--;
+	if (self.loadingViewsCount <= 0) {
+		UIViewController *neighbor = [self.tabBarController.viewControllers firstObject];
+		neighbor.tabBarItem.enabled = YES;
+	}
+}
+@end
