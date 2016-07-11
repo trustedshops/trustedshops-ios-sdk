904af55e20bca004cc884ad4868b89a190c9c72e
Gero Herkenrath <gero.herkenrath@trustedshops.de>
2016-07-11 08:59:22 +0200

    removed compiler flags for deprecation because appledoc is stupid. added better appledoc way (well... "better") to mark deprecated error symbols.

--- Pod/Classes/Public/TRSErrors.h
+++ Pod/Classes/Public/TRSErrors.h
@@ -6,37 +6,43 @@
 
 FOUNDATION_EXPORT NSString * const TRSErrorDomain;
 
-/**	
+/**
  The type for error codes that are used in the Trustbadge SDK. The error domain of each error using these codes
  is defined as `TRSErrorDomain`.
  */
 typedef NS_ENUM(NSUInteger, TRSErrorCode) {
-	/** An unknown error in the context of the Trustbadge SDK */
-    TRSErrorDomainUnknownError = 1000,
-	/** Deprecated, use `TRSErrorDomainUnknownError` instead. */
-	TRSErrorDomainTrustbadgeUnknownError __attribute__((deprecated)) = TRSErrorDomainUnknownError,
+	/** An unknown error occurred in the context of the Trustbadge SDK. */
+	TRSErrorDomainUnknownError = 1000,
+	/** An unknown error occurred in the context of the Trustbadge SDK.
+	 @deprecated use `TRSErrorDomainUnknownError` instead. */
+	TRSErrorDomainTrustbadgeUnknownError = TRSErrorDomainUnknownError,
 	/** The provided Trusted Shops shop ID was rejected as invalid by the API. */
-    TRSErrorDomainInvalidTSID,
-	/** Deprecated, use `TRSErrorDomainInvalidTSID` instead. */
-	TRSErrorDomainTrustbadgeInvalidTSID __attribute__((deprecated)) = TRSErrorDomainInvalidTSID,
+	TRSErrorDomainInvalidTSID,
+	/** The provided Trusted Shops shop ID was rejected as invalid by the API.
+	 @deprecated use `TRSErrorDomainInvalidTSID` instead. */
+	TRSErrorDomainTrustbadgeInvalidTSID = TRSErrorDomainInvalidTSID,
 	/** The provided Trusted Shops shop ID does not exist in the Trusted Shop database. */
-    TRSErrorDomainTSIDNotFound,
-	/** Deprecated, use `TRSErrorDomainTSIDNotFound` instead. */
-	TRSErrorDomainTrustbadgeTSIDNotFound __attribute__((deprecated)) = TRSErrorDomainTSIDNotFound,
-	/** The data returned from the API is corrupted or has an unknown format */
-    TRSErrorDomainInvalidData,
-	/** Deprecated, use `TRSErrorDomainInvalidData` instead. */
-	TRSErrorDomainTrustbadgeInvalidData __attribute__((deprecated)) = TRSErrorDomainInvalidData,
+	TRSErrorDomainTSIDNotFound,
+	/** The provided Trusted Shops shop ID does not exist in the Trusted Shop database.
+	 @deprecated use `TRSErrorDomainTSIDNotFound` instead. */
+	TRSErrorDomainTrustbadgeTSIDNotFound = TRSErrorDomainTSIDNotFound,
+	/** The data returned from the API is corrupted or has an unknown format. */
+	TRSErrorDomainInvalidData,
+	/** The data returned from the API is corrupted or has an unknown format.
+	 @deprecated use `TRSErrorDomainInvalidData` instead. */
+	TRSErrorDomainTrustbadgeInvalidData = TRSErrorDomainInvalidData,
 	/** The data returned from the API is valid, but contains different values than were expected/previously defined. */
 	TRSErrorDomainUnexpectedData,
 	/** The client token for the API is not valid. */
 	TRSErrorDomainInvalidAPIToken,
-	/** Deprecated, use `TRSErrorDomainInvalidAPIToken` instead. */
-	TRSErrorDomainTrustbadgeInvalidAPIToken __attribute__((deprecated)) = TRSErrorDomainInvalidAPIToken,
+	/** The client token for the API is not valid.
+	 @deprecated use `TRSErrorDomainInvalidAPIToken` instead. */
+	TRSErrorDomainTrustbadgeInvalidAPIToken = TRSErrorDomainInvalidAPIToken,
 	/** A call to the API could not be made due to missing TSID or client token (or both were missing). No remote connection was made. */
 	TRSErrorDomainMissingTSIDOrAPIToken,
-	/** Deprecated, use `TRSErrorDomainMissingTSIDOrAPIToken` instead. */
-	TRSErrorDomainTrustbadgeMissingTSIDOrAPIToken __attribute__((deprecated)) = TRSErrorDomainMissingTSIDOrAPIToken,
+	/** A call to the API could not be made due to missing TSID or client token (or both were missing). No remote connection was made.
+	 @deprecated use `TRSErrorDomainMissingTSIDOrAPIToken` instead. */
+	TRSErrorDomainTrustbadgeMissingTSIDOrAPIToken = TRSErrorDomainMissingTSIDOrAPIToken,
 	/** A product grade view was set up without an SKU. Bo remote connection was made. */
 	TRSErrorDomainMissingSKU,
 	/** The processOrder:onCompletion: method could not find a root view controller to present its popup */
